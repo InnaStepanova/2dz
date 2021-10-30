08bd3794537b7a50ebe0da81f633d1b7f9101ede
Inna Stepanova <inna-panichkina@yandex.ru>
2021-10-30 19:46:18 +0300

    UIKit DZ N2

--- 2dz/ViewController.swift
+++ 2dz/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var viewColour: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewColour.layer.cornerRadius = 15
+        setColor()
     }
-
-
+    
+    private func setColor() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        viewColour.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                    green: CGFloat(greenSlider.value),
+                    blue: CGFloat(blueSlider.value),
+                    alpha: 1)
+        
+    }
+    @IBAction func slidersActions() {
+        setColor()
+    }
 }
-
