9254c851f1ec668250039ccf731bcb73c9892670
Inna Stepanova <inna-panichkina@yandex.ru>
2021-10-30 17:34:35 +0300

    Initial Commit

---
+++ 2dz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2dz
+//
+//  Created by Инна Степанова on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
